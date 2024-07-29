12d057ff17282a3025ed1f640686933c772e1f02
pks24680 <pruthvikumar.a.shah@student.uts.edu.au>
2024-07-29 15:54:09 +1000

    Component 1 Testing

--- sc-take-home-assessment-grad/folders/folders_test.go
+++ sc-take-home-assessment-grad/folders/folders_test.go
@@ -2,12 +2,30 @@ package folders_test
 
 import (
 	"testing"
-	// "github.com/georgechieng-sc/interns-2022/folders"
-	// "github.com/stretchr/testify/assert"
+
+	"github.com/georgechieng-sc/interns-2022/folders"
+	"github.com/gofrs/uuid"
+	"github.com/stretchr/testify/assert"
 )
 
 func Test_GetAllFolders(t *testing.T) {
-	t.Run("test", func(t *testing.T) {
-		// your test/s here
+	t.Run("Success", func(t *testing.T) {
+		orgID := uuid.FromStringOrNil(folders.DefaultOrgID)
+		req := &folders.FetchFolderRequest{OrgID: orgID}
+		resp, err := folders.GetAllFolders(req)
+		assert.NoError(t, err)
+		assert.NotNil(t, resp)
+		for _, folder := range resp.Folders {
+			assert.Equal(t, orgID, folder.OrgId)
+		}
+	})
+
+	t.Run("NoFoldersFound", func(t *testing.T) {
+		orgID := uuid.Must(uuid.NewV4())
+		req := &folders.FetchFolderRequest{OrgID: orgID}
+		resp, err := folders.GetAllFolders(req)
+		assert.NoError(t, err)
+		assert.NotNil(t, resp)
+		assert.Empty(t, resp.Folders)
 	})
 }
