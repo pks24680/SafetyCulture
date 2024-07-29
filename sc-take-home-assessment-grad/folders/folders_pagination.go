a969560db691bf3b93051390a942ab3c084f2560
pks24680 <pruthvikumar.a.shah@student.uts.edu.au>
2024-07-29 15:57:33 +1000

    Pagination complete - used struct and GPT to understand some components

--- sc-take-home-assessment-grad/folders/folders_pagination.go
+++ sc-take-home-assessment-grad/folders/folders_pagination.go
@@ -1,3 +1,35 @@
 package folders
 
-// Copy over the `GetFolders` and `FetchAllFoldersByOrgID` to get started
+import (
+	"github.com/gofrs/uuid"
+)
+
+type FetchFolderRequestWithPagination struct {
+	OrgID  uuid.UUID
+	Limit  int
+	Offset int
+}
+
+func GetAllFoldersWithPagination(req *FetchFolderRequestWithPagination) (*FetchFolderResponse, error) {
+	r, err := FetchAllFoldersByOrgID(req.OrgID)
+	if err != nil {
+		return nil, err
+	}
+
+	start := req.Offset
+	end := req.Offset + req.Limit
+
+	if start > len(r) {
+		start = len(r)
+	}
+	if end > len(r) {
+		end = len(r)
+	}
+
+	var fp []*Folder
+	for _, v := range r[start:end] {
+		fp = append(fp, v)
+	}
+
+	return &FetchFolderResponse{Folders: fp}, nil
+}
